0161279d8f27146d26224afc74537c9c75b67812
stefanhermann79@gmail.com <stefanhermann79@gmail.com>
2018-09-07 11:25:59 +0200

    First commit

---
+++ VS1053-music-shield.ino
@@ -0,0 +1,96 @@
+/*************************************************** 
+
+  This file is an updated version to use the Waveshare Music Shield VS1053B with the Adafruit_VS1053 Library.
+  The aim was just to be able to trigger a sound file from the SD – it is not a complete match!
+
+  ....
+  
+  Original comment:
+  
+  This is an example for the Adafruit VS1053 Codec Breakout
+
+  Designed specifically to work with the Adafruit VS1053 Codec Breakout 
+  ----> https://www.adafruit.com/products/1381
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// include SPI, MP3 and SD libraries
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <SD.h>
+
+// These are the pins used for the music maker shield
+#define SHIELD_RESET  A0      // VS1053 reset pin (unused!)
+#define SHIELD_CS     A3      // VS1053 chip select pin (output)
+#define SHIELD_DCS    A2      // VS1053 Data/command select pin (output)
+
+#define CARDCS 10     // Card chip select pin
+#define DREQ A1       // VS1053 Data request, ideally an Interrupt pin – Yes, but not on the shield of Waveshare ...
+
+Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+  
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Adafruit VS1053 Simple Test");
+
+  if (! musicPlayer.begin()) { // initialise the music player
+     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+     while (1);
+  }
+  Serial.println(F("VS1053 found"));
+  
+   if (!SD.begin(CARDCS)) {
+    Serial.println(F("SD failed, or not present"));
+    while (1);  // don't do anything more
+  }
+
+  // list files
+  printDirectory(SD.open("/"), 0);
+  
+  // Set volume for left, right channels. lower numbers == louder volume!
+  musicPlayer.setVolume(40,40);
+  
+  // Play one file, don't return until complete
+  Serial.println(F("Playing track"));
+  
+  musicPlayer.playFullFile("JumpOnIt.mp3"); // this is the file name of a file on the SD card
+  
+}
+
+void loop() {
+
+}
+
+
+/// File listing helper
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+     
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       //Serial.println("**nomorefiles**");
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
+}
+
